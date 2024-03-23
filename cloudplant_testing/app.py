65060217a6139841be92003c1b474f5a064a2bba
Despina Misheva <despina.miseva@gmail.com>
2024-03-23 12:24:32 +0100

    added docker-compose.yml

--- cloudplant_testing/app.py
+++ cloudplant_testing/app.py
@@ -1,19 +1,27 @@
 from flask import Flask, request, jsonify, render_template_string
+import redis
 
 app = Flask(__name__)
 
-# Temporary storage for received data
-received_data = {}
+#ova e za redisot ideme so default porta 
+redis_host = "redis"
+redis_port = 6379
+redis_db = redis.Redis(host=redis_host, port=redis_port)
 
+## ova e obrabotka na post od microcontroller
 @app.route('/data', methods=['POST'])
 def receive_data():
-    global received_data
     data = request.json
-    received_data = data
+    # Store data in Redis
+    for key, value in data.items():
+        redis_db.set(key, value)
     return jsonify(success=True)
 
+##getot za prikazhuvanje na datata
 @app.route('/')
 def index():
+    data = {key.decode('utf-8'): redis_db.get(key).decode('utf-8') for key in redis_db.keys()}
+
     return render_template_string("""
     <!DOCTYPE html>
     <html>
@@ -22,13 +30,22 @@ def index():
     </head>
     <body>
         <h1>Received Sensor Data</h1>
-        <p>Device ID: {{ data.get('deviceID', 'N/A') }}</p>
-        <p>Light Intensity: {{ data.get('lightIntensity', 'N/A') }}</p>
-        <p>Temperature: {{ data.get('temperature', 'N/A') }}°C</p>
-        <p>Humidity: {{ data.get('humidity', 'N/A') }}%</p>
+        <table border="1">
+            <tr>
+                <th>Field</th>
+                <th>Value</th>
+            </tr>
+            {% for key, value in data.items() %}
+            <tr>
+                <td>{{ key }}</td>
+                <td>{{ value }}</td>
+            </tr>
+            {% endfor %}
+        </table>
     </body>
     </html>
-    """, data=received_data)
+    """, data=data)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
